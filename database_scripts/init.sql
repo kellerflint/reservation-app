2e686a3956f5b70220ca582429deac7415cd14f4
Keller Flint <kflint0068@gmail.com>
2024-11-26 11:45:12 -0800

    Update init.sql

--- database_scripts/init.sql
+++ database_scripts/init.sql
@@ -1,15 +1,19 @@
 -- CREATE DATABASE reservations;
-
 USE reservations;
 
 DROP TABLE IF EXISTS users;
+
 CREATE TABLE users(
 	id INT AUTO_INCREMENT,
     firstName VARCHAR(255),
     lastName VARCHAR(255),
+    email VARCHAR(255),
     timestamp DATETIME DEFAULT NOW(),
     
     PRIMARY KEY (id)
 );
 
-INSERT INTO users (firstName, lastName) VALUES ('John', 'Doe');
+INSERT INTO users (firstName, lastName, email) 
+VALUES ('John', 'Doe', "j@gmail.com");
+
+SELECT * FROM users;
