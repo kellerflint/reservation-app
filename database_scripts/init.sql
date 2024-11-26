1424eac1d75f20e2157d279e6513246e3ac92927
Keller Flint <kflint0068@gmail.com>
2024-11-26 11:45:26 -0800

    Update init.sql

--- database_scripts/init.sql
+++ database_scripts/init.sql
@@ -4,7 +4,7 @@ USE reservations;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-	id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     firstName VARCHAR(255),
     lastName VARCHAR(255),
     email VARCHAR(255),
